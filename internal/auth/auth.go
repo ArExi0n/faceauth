66d2a71a5236ca76b4175fb176428a40ee3f5853
ArExi0n <notbollt@gmail.com>
2026-06-29 01:04:17 +0530

    auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,40 @@
 package auth
 
-import(
+import (
 	"context"
-	"github.com/"
+	"faceauth/internal/store"
 )
+
+type Authenticator interface {
+	GenerateToken(context.Context) (string, error)
+	ValidateToken(context.Context) error
+}
+
+type SessionAuth struct {
+	store store.Storage
+}
+
+func New(store store.Storage) *SessionAuth {
+	return &SessionAuth{
+		store: store,
+	}
+}
+
+func (sa *SessionAuth) GenerateToken(ctx context.Context) (string, error) {
+	sessinToken, err := sa.store.Session.Create(ctx)
+
+	if err != nil {
+		return "", err
+	}
+	return sessinToken.Token, err
+}
+
+func (sa *SessionAuth) ValidateToken(ctx context.Context, token string) error {
+	_, err := sa.store.Session.FindBySessionToken(ctx, token)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
